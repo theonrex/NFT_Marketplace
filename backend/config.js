3592bdb2e63b7fa55a3b2a73a4b4582287a2e25f
Armstrong Olawande <theonrex1@gmail.com>
2022-11-10 20:34:41 +0100

    valist build and export

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 
-  export const nftMarketplaceAddress = "0xe5475BC7ce42c2169D39930caD2b614e1dDe3ba1"
-  export const nftAddress = "0xDd5d091a9B28c8363CdBFc954f110b49bb410bb7"
+  // export const nftMarketplaceAddress = "0xe5475BC7ce42c2169D39930caD2b614e1dDe3ba1"
+  // export const nftAddress = "0xDd5d091a9B28c8363CdBFc954f110b49bb410bb7"
   
--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -7,38 +7,22 @@
  require("@nomiclabs/hardhat-ethers");
  require("dotenv").config();
  const fs = require("fs");
+
  
- 
- 
- const { API_URL_KEY, PRIVATE_KEY } = process.env;
+ const {QUICKNODE_API_URL_KEY, PRIVATE_KEY } = process.env;
  
  module.exports = {
    networks: {
      hardhat: {
-       chainId: 80001,
+       chainId: 5,
      },
-     mumbai: {
-       url: API_URL_KEY,
+     goerli: {
+       url: QUICKNODE_API_URL_KEY,
        accounts: [PRIVATE_KEY],
      },
-  
-     // mainnet: {
-     //   url: "https://polygon-mainnet.g.alchemy.com/v2/_WH8c3Rc9mrWUJ2GFWWjKR16TiPi7Ign",
-     //   accounts:[PRIVATE_KEY],
-     // },
    },
    solidity: "0.8.4",
-   namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-    player: {
-      default: 1,
-    },
-  },
-  mocha: {
-    timeout: 40000
-  }
+
  };
  
  
